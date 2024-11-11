a37ea695874e99f9bb9a74f276b177a689b6e5ce
domi413 <dom.buehler@outlook.com>
2024-11-11 13:08:04 +0100

    prevent error message

--- lua/telescope/_extensions/telescope-tabs/main.lua
+++ lua/telescope/_extensions/telescope-tabs/main.lua
@@ -95,8 +95,7 @@ M.list_tabs = function(opts)
 		local window_ids = {}
 		local is_current = current_tab.number == vim.api.nvim_tabpage_get_number(tid)
 		for _, wid in ipairs(vim.api.nvim_tabpage_list_wins(tid)) do
-			-- Only consider the normal windows and ignore the floating windows
-			if vim.api.nvim_win_get_config(wid).relative == "" then
+			if vim.api.nvim_win_get_config(wid).relative == '' then
 				local bid = vim.api.nvim_win_get_buf(wid)
 				local path = vim.api.nvim_buf_get_name(bid)
 				local file_name = vim.fn.fnamemodify(path, ':t')
@@ -132,7 +131,9 @@ M.list_tabs = function(opts)
 				actions.select_default:replace(function()
 					actions.close(prompt_bufnr)
 					local selection = action_state.get_selected_entry()
-					vim.api.nvim_set_current_tabpage(selection.value[5])
+					if selection then
+						vim.api.nvim_set_current_tabpage(selection.value[5])
+					end
 				end)
 				map('i', opts.close_tab_shortcut_i, close_tab)
 				map('n', opts.close_tab_shortcut_n, close_tab)
